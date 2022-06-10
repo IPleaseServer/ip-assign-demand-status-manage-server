c6b00e891c9ea8679eed9addd9f67440740b471d
raul <develop.raul@gmail.com>
2022-06-10 15:26:06 +0900

    Initial commit

---
+++ src/main/java/site/iplease/iadsmserver/IpAssignDemandStatusManageServerApplication.java
@@ -0,0 +1,13 @@
+package site.iplease.iadsmserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpAssignDemandStatusManageServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IpAssignDemandStatusManageServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/site/iplease/iadsmserver/IpAssignDemandStatusManageServerApplicationTests.java
@@ -0,0 +1,13 @@
+package site.iplease.iadsmserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IpAssignDemandStatusManageServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
