4de87b54b00907a35eecc9c82ec70a544c2492f0
raul <develop.raul@gmail.com>
2022-06-12 00:58:45 +0900

    [ADD] Subscriber에 대한 진입지점인 RabbitMqListener 작성 및 이에 필요한 TypeConstant작성
    - MessageType에 IP_ASSIGN_DEMAND_CREATE 추가
    - MessageType에 routingKey로 MessageType을 찾는 of 정적메서드 작성

--- src/main/java/site/iplease/iadsmserver/infra/message/type/MessageType.kt
+++ src/main/java/site/iplease/iadsmserver/infra/message/type/MessageType.kt
@@ -1,5 +1,10 @@
 package site.iplease.iadsmserver.infra.message.type
 
 enum class MessageType(val routingKey: String) {
-    DEMAND_ERROR_ON_STATUS("ipAssignDemandErrorOnStatus")
+    IP_ASSIGN_DEMAND_CREATE("ipAssignDemandCreate"),
+    DEMAND_ERROR_ON_STATUS("ipAssignDemandErrorOnStatus");
+
+    companion object {
+        fun of(routingKey: String) = values().first { it.routingKey == routingKey }
+    }
 }
