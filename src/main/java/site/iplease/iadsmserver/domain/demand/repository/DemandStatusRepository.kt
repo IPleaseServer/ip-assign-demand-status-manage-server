949848eb04d2cb01b1e5ad11b9440e9b8f8f7bbb
raul <develop.raul@gmail.com>
2022-06-11 16:54:39 +0900

    [ADD] DemandStatusValidatorV1구현 및 이에 필요한 인터페이스 스팩 및 예외 추가
    - DemandStatusAlreadyExistsException 작성
    - WrongDemandStatusTypeException 작성
    - DemandStatusRepository에 existsByDemandId스팩 추가

--- src/main/java/site/iplease/iadsmserver/domain/demand/repository/DemandStatusRepository.kt
+++ src/main/java/site/iplease/iadsmserver/domain/demand/repository/DemandStatusRepository.kt
@@ -1,6 +1,9 @@
 package site.iplease.iadsmserver.domain.demand.repository
 
 import org.springframework.data.repository.reactive.ReactiveCrudRepository
+import reactor.core.publisher.Mono
 import site.iplease.iadsmserver.domain.demand.entity.DemandStatus
 
-interface DemandStatusRepository: ReactiveCrudRepository<DemandStatus, Long>
+interface DemandStatusRepository: ReactiveCrudRepository<DemandStatus, Long> {
+    fun existsByDemandId(demandId: Long): Mono<Boolean>
+}
---
+++ src/main/java/site/iplease/iadsmserver/domain/demand/util/DemandStatusValidatorV1.kt
@@ -0,0 +1,35 @@
+package site.iplease.iadsmserver.domain.demand.util
+
+import org.springframework.stereotype.Component
+import reactor.core.publisher.Mono
+import reactor.kotlin.core.publisher.toMono
+import site.iplease.iadsmserver.domain.demand.dto.DemandStatusDto
+import site.iplease.iadsmserver.domain.demand.exception.DemandStatusAlreadyExistsException
+import site.iplease.iadsmserver.domain.demand.exception.WrongDemandStatusTypeException
+import site.iplease.iadsmserver.domain.demand.repository.DemandStatusRepository
+import site.iplease.iadsmserver.domain.demand.type.DemandStatusPolicyGroup
+import site.iplease.iadsmserver.domain.demand.type.DemandStatusType
+
+@Component
+class DemandStatusValidatorV1(
+    private val demandStatusRepository: DemandStatusRepository
+): DemandStatusValidator {
+    override fun validate(policy: DemandStatusPolicyGroup, demand: DemandStatusDto): Mono<DemandStatusDto> =
+        policy.toMono().flatMap {
+            when(it) {DemandStatusPolicyGroup.CREATE -> validateNotExists(demand).flatMap { validateStatusCreate(demand) } }
+        }.map { demand }
+
+    //해당 예약이 존재하지 않아야한다.
+    private fun validateNotExists(demand: DemandStatusDto) =
+        demandStatusRepository.existsByDemandId(demand.demandId)
+            .flatMap {
+                if(it) Mono.error(DemandStatusAlreadyExistsException("해당 예약ID를 가지는 예약상태정보가 이미 존재합니다!", demand.demandId))
+                else Unit.toMono()
+            }
+
+    private fun validateStatusCreate(demand: DemandStatusDto) =
+        demand.toMono().flatMap {
+                if(demand.status != DemandStatusType.CREATE) Mono.error(WrongDemandStatusTypeException("예약 생성시 예약의 상태는 CREATE이어야합니다.", demand.status))
+                else Unit.toMono()
+            }
+}
