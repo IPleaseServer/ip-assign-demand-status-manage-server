1cca7c0bfa5307868c8b61a06339b4d7dc60ac78
raul <develop.raul@gmail.com>
2022-06-12 01:03:12 +0900

    [FIX] 처리대상이 아닌 메세지 바인딩시 오류 핸들링

--- src/main/java/site/iplease/iadsmserver/infra/message/type/MessageType.kt
+++ src/main/java/site/iplease/iadsmserver/infra/message/type/MessageType.kt
@@ -2,9 +2,12 @@ package site.iplease.iadsmserver.infra.message.type
 
 enum class MessageType(val routingKey: String) {
     IP_ASSIGN_DEMAND_CREATE("ipAssignDemandCreate"),
-    DEMAND_ERROR_ON_STATUS("ipAssignDemandErrorOnStatus");
+    DEMAND_ERROR_ON_STATUS("ipAssignDemandErrorOnStatus"),
+    UNKNOWN("");
 
     companion object {
-        fun of(routingKey: String) = values().first { it.routingKey == routingKey }
+        fun of(routingKey: String) =
+            kotlin.runCatching { values().first { it.routingKey == routingKey } }
+            .getOrElse { UNKNOWN }
     }
 }
