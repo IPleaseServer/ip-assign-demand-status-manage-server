77d3b61d31af6ad810c67e518e398550881737bb
raul <develop.raul@gmail.com>
2022-06-15 10:41:05 +0900

    [ADD] DemandStatusValidator 구현 및 이에 필요한 객체 작성
    - DemandStatusType에 isCancellable메서드 추가
    - UnCancellableDemandStatusException작성

--- src/main/java/site/iplease/iadsmserver/domain/demand/util/DemandStatusValidatorV1.kt
+++ src/main/java/site/iplease/iadsmserver/domain/demand/util/DemandStatusValidatorV1.kt
@@ -9,6 +9,7 @@ import site.iplease.iadsmserver.domain.demand.exception.WrongDemandStatusTypeExc
 import site.iplease.iadsmserver.domain.demand.repository.DemandStatusRepository
 import site.iplease.iadsmserver.domain.demand.data.type.DemandStatusPolicyGroup
 import site.iplease.iadsmserver.domain.demand.data.type.DemandStatusType
+import site.iplease.iadsmserver.domain.demand.exception.UnCancellableDemandStatusException
 
 @Component
 class DemandStatusValidatorV1(
@@ -17,20 +18,27 @@ class DemandStatusValidatorV1(
     override fun validate(policy: DemandStatusPolicyGroup, demand: DemandStatusDto): Mono<DemandStatusDto> =
         policy.toMono().flatMap {
             when(it!!) {
-                DemandStatusPolicyGroup.CREATE -> validateNotExists(demand).flatMap { validateStatusCreate(demand) } }
+                DemandStatusPolicyGroup.CREATE -> validateExists(demand, false).flatMap { validateStatus(demand, DemandStatusType.CREATE) }
+                DemandStatusPolicyGroup.CANCEL -> validateExists(demand).flatMap { validateStatusCancellable(demand) }
+            }
         }.map { demand }
 
-    //해당 예약이 존재하지 않아야한다.
-    private fun validateNotExists(demand: DemandStatusDto) =
+    private fun validateExists(demand: DemandStatusDto, beExists: Boolean = true) =
         demandStatusRepository.existsByDemandId(demand.demandId)
             .flatMap {
-                if(it) Mono.error(DemandStatusAlreadyExistsException("해당 예약ID를 가지는 예약상태정보가 이미 존재합니다!", demand.demandId))
-                else Unit.toMono()
+                if(it == beExists) Unit.toMono()
+                else Mono.error(DemandStatusAlreadyExistsException("해당 예약ID를 가지는 예약상태정보가 이미 존재합니다!", demand.demandId))
             }
 
-    private fun validateStatusCreate(demand: DemandStatusDto) =
+    private fun validateStatus(demand: DemandStatusDto, beStatus: DemandStatusType) =
         demand.toMono().flatMap {
-                if(demand.status != DemandStatusType.CREATE) Mono.error(WrongDemandStatusTypeException("예약 생성시 예약의 상태는 CREATE이어야합니다.", demand.status))
-                else Unit.toMono()
+                if(demand.status == beStatus) Unit.toMono()
+                else Mono.error(WrongDemandStatusTypeException("예약 생성시 예약의 상태는 CREATE이어야합니다.", demand.status))
             }
+
+    private fun validateStatusCancellable(demand: DemandStatusDto) =
+        demand.toMono().flatMap {
+            if(DemandStatusType.isCancellable(demand.status)) Unit.toMono()
+            else Mono.error(UnCancellableDemandStatusException("취소할 수 없는 예약상태정보입니다. - ${demand.status}"))
+        }
 }
