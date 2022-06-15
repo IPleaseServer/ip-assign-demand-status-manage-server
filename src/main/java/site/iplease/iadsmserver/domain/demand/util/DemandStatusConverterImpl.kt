e68a231c30be93ea3190c6d207a151c40fdfd884
raul <develop.raul@gmail.com>
2022-06-15 10:18:54 +0900

    [ADD] DemandStatusConverter에 추가된 스팩 구현

--- src/main/java/site/iplease/iadsmserver/domain/demand/util/DemandStatusConverterImpl.kt
+++ src/main/java/site/iplease/iadsmserver/domain/demand/util/DemandStatusConverterImpl.kt
@@ -3,12 +3,17 @@ package site.iplease.iadsmserver.domain.demand.util
 import org.springframework.stereotype.Component
 import reactor.core.publisher.Mono
 import reactor.kotlin.core.publisher.toMono
+import site.iplease.iadsmserver.domain.demand.data.dto.DemandDto
 import site.iplease.iadsmserver.domain.demand.data.dto.DemandStatusDto
 import site.iplease.iadsmserver.domain.demand.data.entity.DemandStatus
 import site.iplease.iadsmserver.global.demand.message.IpAssignDemandCreateMessage
 
 @Component
 class DemandStatusConverterImpl: DemandStatusConverter {
+    override fun toDto(demand: DemandDto): Mono<DemandStatusDto> =
+        demand.toMono()
+            .map { DemandStatusDto(demandId = demand.id) }
+
     override fun toDto(message: IpAssignDemandCreateMessage) =
         message.toMono()
             .map { DemandStatusDto(
