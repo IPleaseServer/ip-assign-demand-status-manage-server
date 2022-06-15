5e108877d7d427eaefbe6f9d592def87303401d1
raul <develop.raul@gmail.com>
2022-06-15 11:21:23 +0900

    [ADD] RabbitMqListener에 IpAssignDemandCancelSubscriber등록 및 이에따른 MessageType Constant 추가 - MessageType에 IP_ASSIGN_DEMAND_CANCEL Constant추가

--- src/main/java/site/iplease/iadsmserver/infra/message/listener/RabbitMqListener.kt
+++ src/main/java/site/iplease/iadsmserver/infra/message/listener/RabbitMqListener.kt
@@ -7,13 +7,16 @@ import org.springframework.amqp.rabbit.annotation.RabbitListener
 import org.springframework.stereotype.Component
 import reactor.core.publisher.Mono
 import reactor.kotlin.core.publisher.toMono
+import site.iplease.iadsmserver.global.demand.message.IpAssignDemandCancelMessage
 import site.iplease.iadsmserver.global.demand.subscriber.IpAssignDemandCreateSubscriber
 import site.iplease.iadsmserver.global.demand.message.IpAssignDemandCreateMessage
+import site.iplease.iadsmserver.global.demand.subscriber.IpAssignDemandCancelSubscriber
 import site.iplease.iadsmserver.infra.message.type.MessageType
 
 @Component
 class RabbitMqListener(
     private val ipAssignDemandCreateSubscriber: IpAssignDemandCreateSubscriber,
+    private val ipAssignDemandCancelSubscriber: IpAssignDemandCancelSubscriber,
     private val objectMapper: ObjectMapper
 ) {
     private val logger = LoggerFactory.getLogger(this::class.java)
@@ -21,24 +24,30 @@ class RabbitMqListener(
     @RabbitListener(queues = ["iplease.ip.assign.demand.status.manage"])
     fun listen(message: Message) {
         val routingKey = message.messageProperties.receivedRoutingKey
-        when(MessageType.of(routingKey)) {
-            MessageType.IP_ASSIGN_DEMAND_CREATE ->
-                objectMapper.toMono()
-                    .map { it.readValue(String(message.body), IpAssignDemandCreateMessage::class.java) }
-                    .map { ipAssignDemandCreateSubscriber.subscribe(message = it) }
-                    .doOnError{ throwable ->
-                        logger.error("메세지를 직렬화하는도중 오류가 발생하였습니다!")
-                        logger.error("exception: ${throwable.localizedMessage}")
-                        logger.error("payload(string): ${String(message.body)}")
-                        logger.error("payload(byte): ${message.body}")
-                    }.onErrorResume { Mono.empty() }
-                    .block()
+        val payload = message.body.toString()
+        handleMessage(MessageType.of(routingKey), payload)
+            .doOnError{ throwable ->
+                logger.error("메세지를 직렬화하는도중 오류가 발생하였습니다!")
+                logger.error("exception: ${throwable.localizedMessage}")
+                logger.error("payload(string): ${String(message.body)}")
+                logger.error("payload(byte): ${message.body}")
+            }.onErrorResume { Mono.empty() }
+            .block()
+    }
+
+    private fun handleMessage(type: MessageType, payload: String): Mono<Unit> =
+        when(type) {
+            MessageType.IP_ASSIGN_DEMAND_CREATE -> objectMapper.toMono()
+                .map { it.readValue(payload, IpAssignDemandCreateMessage::class.java) }
+                .map { message -> ipAssignDemandCreateSubscriber.subscribe(message) }
+            MessageType.IP_ASSIGN_DEMAND_CANCEL -> objectMapper.toMono()
+                .map { it.readValue(payload, IpAssignDemandCancelMessage::class.java) }
+                .map { message -> ipAssignDemandCancelSubscriber.subscribe(message) }
             else -> {
                 logger.warn("처리대상이 아닌 메세지가 바인딩되어있습니다!")
-                logger.warn("routingKey: ${message.messageProperties.receivedRoutingKey}")
-                logger.warn("payload(string): ${String(message.body)}")
-                logger.warn("payload(byte): ${message.body}")
+                logger.warn("routingKey: ${type.routingKey}")
+                logger.warn("payload(string): $payload")
+                Unit.toMono()
             }
         }
-    }
 }
--- src/main/java/site/iplease/iadsmserver/infra/message/type/MessageType.kt
+++ src/main/java/site/iplease/iadsmserver/infra/message/type/MessageType.kt
@@ -2,6 +2,7 @@ package site.iplease.iadsmserver.infra.message.type
 
 enum class MessageType(val routingKey: String) {
     IP_ASSIGN_DEMAND_CREATE("ipAssignDemandCreate"),
+    IP_ASSIGN_DEMAND_CANCEL("ipAssignDemandCancel"),
     DEMAND_ERROR_ON_STATUS("ipAssignDemandErrorOnStatus"),
     SEND_ALARM("sendAlarm"),
     UNKNOWN("");
