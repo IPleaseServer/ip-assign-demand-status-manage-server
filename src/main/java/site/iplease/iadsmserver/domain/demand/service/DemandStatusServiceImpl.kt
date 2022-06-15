e3594c2099a4b441ee23a586b7240f1fca92db15
raul <develop.raul@gmail.com>
2022-06-15 10:17:04 +0900

    [ADD] DemandStatusService에 추가된 스팩 구현

--- src/main/java/site/iplease/iadsmserver/domain/demand/service/DemandStatusServiceImpl.kt
+++ src/main/java/site/iplease/iadsmserver/domain/demand/service/DemandStatusServiceImpl.kt
@@ -2,6 +2,7 @@ package site.iplease.iadsmserver.domain.demand.service
 
 import org.springframework.stereotype.Service
 import reactor.core.publisher.Mono
+import reactor.kotlin.core.publisher.toMono
 import site.iplease.iadsmserver.domain.demand.data.dto.DemandStatusDto
 import site.iplease.iadsmserver.domain.demand.repository.DemandStatusRepository
 import site.iplease.iadsmserver.domain.demand.util.DemandStatusConverter
@@ -11,8 +12,11 @@ class DemandStatusServiceImpl(
     private val demandStatusConverter: DemandStatusConverter,
     private val demandStatusRepository: DemandStatusRepository
 ): DemandStatusService {
-    override fun add(demand: DemandStatusDto): Mono<DemandStatusDto> =
-        demandStatusConverter.toEntity(demand)//Dto 를 Entity로 전환한다.
+    override fun add(demandStatus: DemandStatusDto): Mono<DemandStatusDto> =
+        demandStatusConverter.toEntity(demandStatus)//Dto 를 Entity로 전환한다.
             .flatMap { entity -> demandStatusRepository.save(entity) }//전환한 Entity를 영속성 컨텍스트에 저장한다.
             .flatMap { entity -> demandStatusConverter.toDto(entity) }//저장한 Entity를 Dto로 전환하여 반환한다.
+
+    override fun remove(demandStatus: DemandStatusDto): Mono<Unit> =
+            demandStatusRepository.deleteById(demandStatus.id).then(Unit.toMono())
 }
