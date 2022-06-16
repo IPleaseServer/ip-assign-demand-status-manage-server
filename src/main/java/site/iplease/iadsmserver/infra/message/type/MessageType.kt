84376843f5a0b0b9937dfbd686971f9f555ac568
raul <develop.raul@gmail.com>
2022-06-16 09:28:21 +0900

    [FIX] IP_ASSIGN_DEMAND_CREATE_ERROR_ON_STATUS routing key 수정

--- src/main/java/site/iplease/iadsmserver/infra/message/type/MessageType.kt
+++ src/main/java/site/iplease/iadsmserver/infra/message/type/MessageType.kt
@@ -3,7 +3,7 @@ package site.iplease.iadsmserver.infra.message.type
 enum class MessageType(val routingKey: String) {
     IP_ASSIGN_DEMAND_CREATE("ipAssignDemandCreate"),
     IP_ASSIGN_DEMAND_CANCEL("ipAssignDemandCancel"),
-    IP_ASSIGN_DEMAND_CREATE_ERROR_ON_STATUS("ipAssignDemandErrorOnStatus"),//TODO routingKey를 ipAssignDemandCreateErrorOnStatus로 변경
+    IP_ASSIGN_DEMAND_CREATE_ERROR_ON_STATUS("ipAssignDemandCreateErrorOnStatus"),
     IP_ASSIGN_DEMAND_CANCEL_ERROR_ON_STATUS("ipAssignDemandCancelErrorOnStatus"),
     SEND_ALARM("sendAlarm"),
     UNKNOWN("");
