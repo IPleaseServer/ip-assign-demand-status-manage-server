abfa46d94236e961708e2a971afe3dc5a1255bc8
raul <develop.raul@gmail.com>
2022-06-16 09:14:19 +0900

    [FIX] 잘못된 Message를 발행하는 IpAssignDemandCancelSubscriberV1 로직 수정

--- src/main/java/site/iplease/iadsmserver/domain/demand/subscriber/IpAssignDemandCreateSubscriberV1.kt
+++ src/main/java/site/iplease/iadsmserver/domain/demand/subscriber/IpAssignDemandCreateSubscriberV1.kt
@@ -23,7 +23,7 @@ class IpAssignDemandCreateSubscriberV1(
             .flatMap { pushAlarmService.publish(message.issuerId, "신청이 등록됬어요!", "담당 선생님이 신청을 확인하시면 다시 알려드릴게요 :)") }//추가 성공시, 유저에게 푸시알람을 전송한다.
             .onErrorResume { throwable -> //추가 실패시 예약추가 실패 메세지를 전파한다.
                 IpAssignDemandErrorOnStatusMessage(demandId = message.demandId, issuerId = message.issuerId, message = throwable.localizedMessage)
-                    .let { messagePublishService.publish(MessageType.DEMAND_ERROR_ON_STATUS, it) }
+                    .let { messagePublishService.publish(MessageType.IP_ASSIGN_DEMAND_CREATE_ERROR_ON_STATUS, it) }
             }.block()
     }
 }
--- src/main/java/site/iplease/iadsmserver/infra/message/type/MessageType.kt
+++ src/main/java/site/iplease/iadsmserver/infra/message/type/MessageType.kt
@@ -3,7 +3,8 @@ package site.iplease.iadsmserver.infra.message.type
 enum class MessageType(val routingKey: String) {
     IP_ASSIGN_DEMAND_CREATE("ipAssignDemandCreate"),
     IP_ASSIGN_DEMAND_CANCEL("ipAssignDemandCancel"),
-    DEMAND_ERROR_ON_STATUS("ipAssignDemandErrorOnStatus"),
+    IP_ASSIGN_DEMAND_CREATE_ERROR_ON_STATUS("ipAssignDemandErrorOnStatus"),//TODO routingKey를 ipAssignDemandCreateErrorOnStatus로 변경
+    IP_ASSIGN_DEMAND_CANCEL_ERROR_ON_STATUS("ipAssignDemandCancelErrorOnStatus"),
     SEND_ALARM("sendAlarm"),
     UNKNOWN("");
 
