f49e1f8f1817b93c554348c4da5e983b372965a3
raul <develop.raul@gmail.com>
2022-06-17 01:24:35 +0900

    [ADD] DemandStatusValidator 추가스팩 구현 및 이에 필요한 인터페이스 스팩추가
    - DemandStatusRepository에 findByDemandId메서드 스팩 추가
    - DemandStatusType에 Changeable관련 스팩 추가
    - UnChangeableDemandStatusException 작성

--- src/main/java/site/iplease/iadsmserver/domain/demand/util/DemandStatusValidatorV1.kt
+++ src/main/java/site/iplease/iadsmserver/domain/demand/util/DemandStatusValidatorV1.kt
@@ -4,13 +4,10 @@ import org.springframework.stereotype.Component
 import reactor.core.publisher.Mono
 import reactor.kotlin.core.publisher.toMono
 import site.iplease.iadsmserver.domain.demand.data.dto.DemandStatusDto
-import site.iplease.iadsmserver.domain.demand.exception.DemandStatusAlreadyExistsException
-import site.iplease.iadsmserver.domain.demand.exception.WrongDemandStatusTypeException
 import site.iplease.iadsmserver.domain.demand.repository.DemandStatusRepository
 import site.iplease.iadsmserver.domain.demand.data.type.DemandStatusPolicyGroup
 import site.iplease.iadsmserver.domain.demand.data.type.DemandStatusType
-import site.iplease.iadsmserver.domain.demand.exception.UnCancellableDemandStatusException
-import site.iplease.iadsmserver.domain.demand.exception.UnknownDemandStatusException
+import site.iplease.iadsmserver.domain.demand.exception.*
 
 @Component
 class DemandStatusValidatorV1(
@@ -21,8 +18,9 @@ class DemandStatusValidatorV1(
             when(it!!) {
                 DemandStatusPolicyGroup.CREATE -> validateExists(demand, false).flatMap { validateStatus(demand, DemandStatusType.CREATE) }
                 DemandStatusPolicyGroup.CANCEL -> validateExists(demand).flatMap { validateStatusCancellable(demand) }
+                DemandStatusPolicyGroup.CONFIRM -> validateExists(demand).flatMap { validateStatusChangeable(demand, DemandStatusType.CONFIRM) }
             }
-        }.map { demand }
+        }.flatMap { demandStatusRepository.findByDemandId(demand.demandId) }
 
     private fun validateExists(demand: DemandStatusDto, beExists: Boolean = true) =
         demandStatusRepository.existsByDemandId(demand.demandId)
@@ -35,12 +33,17 @@ class DemandStatusValidatorV1(
     private fun validateStatus(demand: DemandStatusDto, beStatus: DemandStatusType) =
         demand.toMono().flatMap {
                 if(demand.status == beStatus) Unit.toMono()
-                else Mono.error(WrongDemandStatusTypeException("예약 생성시 예약의 상태는 CREATE이어야합니다.", demand.status))
+                else Mono.error(WrongDemandStatusTypeException("예약의 상태는 ${beStatus}여야합니다!", demand.status))
             }
 
     private fun validateStatusCancellable(demand: DemandStatusDto) =
         demand.toMono().flatMap {
             if(DemandStatusType.isCancellable(demand.status)) Unit.toMono()
             else Mono.error(UnCancellableDemandStatusException("취소할 수 없는 예약상태정보입니다. - ${demand.status}"))
+        }
+    private fun validateStatusChangeable(demand: DemandStatusDto, beStatus: DemandStatusType) =
+        demandStatusRepository.findByDemandId(demand.demandId).flatMap { dto ->
+            if(dto.status.isChangeable(beStatus)) Unit.toMono()
+            else Mono.error(UnChangeableDemandStatusException("${dto.status}상태는 ${beStatus}상태로 변경할 수 없습니다. - ${demand.demandId}"))
         }
 }
