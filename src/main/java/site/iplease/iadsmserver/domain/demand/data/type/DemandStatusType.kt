a322ed865a90c66e0c4380a4deb87d3e7bc1cde5
raul <develop.raul@gmail.com>
2022-06-16 18:08:32 +0900

    [ADD] DemandStatusService구현 및 이에필요한 객체 작성
    - DemandStatusConverter 스팩추가
    - DemandStatusPolicyGroup에 Constant CONFIRM 추가
    - DemandStatusType에 CONFIRM 추가

--- src/main/java/site/iplease/iadsmserver/domain/demand/data/type/DemandStatusType.kt
+++ src/main/java/site/iplease/iadsmserver/domain/demand/data/type/DemandStatusType.kt
@@ -2,7 +2,7 @@ package site.iplease.iadsmserver.domain.demand.data.type
 
 //TODO DemandStatusType으로 이름변경
 enum class DemandStatusType {
-    CREATE;
+    CREATE, CONFIRM;
 
     companion object {
         private val cancellable: Set<DemandStatusType> = setOf(CREATE)
