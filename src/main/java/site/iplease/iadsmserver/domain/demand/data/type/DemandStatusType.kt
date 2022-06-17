861ac7d58df32f4d018c0ff7b91e1e067e3cc53c
raul <develop.raul@gmail.com>
2022-06-17 22:14:44 +0900

    [ADD] DemandStatusService 추가스팩 구현 및 이에 필요한 객체 작성
    - DemandStatusPolicyGroup REJECT Constant작성
    - DemandStatusType REJECT Constant작성
    - DemandRejectReasonValidator 인터페이스 작성

--- src/main/java/site/iplease/iadsmserver/domain/demand/data/type/DemandStatusType.kt
+++ src/main/java/site/iplease/iadsmserver/domain/demand/data/type/DemandStatusType.kt
@@ -2,7 +2,7 @@ package site.iplease.iadsmserver.domain.demand.data.type
 
 //TODO DemandStatusType으로 이름변경
 enum class DemandStatusType {
-    CREATE, CONFIRM;
+    CREATE, CONFIRM, REJECT;
     fun isChangeable(status: DemandStatusType): Boolean = Companion.isChangeable(this, status)
 
     companion object {
@@ -10,7 +10,8 @@ enum class DemandStatusType {
         //initialize 문제로인해, 아래와 같은 방식으로 binding합니다.
         private val changeable: Map<DemandStatusType, Set<DemandStatusType>> = mapOf(
             CREATE to setOf(CONFIRM),
-            CONFIRM to setOf(CREATE)
+            CONFIRM to setOf(CREATE, REJECT),
+            REJECT to setOf()
         )
         fun isCancellable(status: DemandStatusType): Boolean = cancellable.contains(status)
         fun isChangeable(origin: DemandStatusType, tobe: DemandStatusType): Boolean = changeable[origin]!!.contains(tobe)
