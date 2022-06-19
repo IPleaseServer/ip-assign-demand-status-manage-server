8ef48a50515d29204306634f57db634c34db9732
raul <develop.raul@gmail.com>
2022-06-20 08:06:04 +0900

    [ADD] DemandStatusConverter구현

--- src/main/java/site/iplease/iadsmserver/global/demand/message/IpAssignDemandAcceptMessage.kt
+++ src/main/java/site/iplease/iadsmserver/global/demand/message/IpAssignDemandAcceptMessage.kt
@@ -3,8 +3,8 @@ package site.iplease.iadsmserver.global.demand.message
 import site.iplease.iadsmserver.domain.demand.data.type.DemandStatusType
 
 data class IpAssignDemandAcceptMessage (
-    val issuerId: Long,
-    val assignIp: String,
     val demandId: Long,
-    val originStatus: DemandStatusType
+    val issuerId: Long,
+    val originStatus: DemandStatusType,
+    val assignIp: String,
 )
