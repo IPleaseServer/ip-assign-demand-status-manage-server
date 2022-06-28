34de09779583ff73d028f4acba33581af3068898
raul <develop.raul@gmail.com>
2022-06-28 12:03:38 +0900

    [ADD] ControllerAdvice에 UnknownDemandStatusException Handling로직 추가

--- src/main/java/site/iplease/iadsmserver/domain/demand/advice/IpAssignDemandStatusControllerAdvice.kt
+++ src/main/java/site/iplease/iadsmserver/domain/demand/advice/IpAssignDemandStatusControllerAdvice.kt
@@ -8,11 +8,18 @@ import reactor.kotlin.core.publisher.toMono
 import site.iplease.iadsmserver.domain.demand.exception.DemandStatusAlreadyExistsException
 import site.iplease.iadsmserver.domain.demand.exception.MalformedIpException
 import site.iplease.iadsmserver.domain.demand.exception.UnChangeableDemandStatusException
+import site.iplease.iadsmserver.domain.demand.exception.UnknownDemandStatusException
 
 @RestControllerAdvice
 class IpAssignDemandStatusControllerAdvice {
     @ExceptionHandler(DemandStatusAlreadyExistsException::class)
     fun handle(exception: DemandStatusAlreadyExistsException): Mono<ResponseEntity<String>> =
+        ResponseEntity.badRequest()
+            .body(exception.localizedMessage)
+            .toMono()
+
+    @ExceptionHandler(UnknownDemandStatusException::class)
+    fun handle(exception: UnknownDemandStatusException): Mono<ResponseEntity<String>> =
         ResponseEntity.badRequest()
             .body(exception.localizedMessage)
             .toMono()
