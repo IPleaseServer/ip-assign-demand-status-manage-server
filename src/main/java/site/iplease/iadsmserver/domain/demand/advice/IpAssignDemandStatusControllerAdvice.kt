f0132a5e0ef4fc05d7d0a855580f050466264ead
raul <develop.raul@gmail.com>
2022-08-09 19:12:24 +0900

    [ADD] 기존 로직에 error 컴포넌트 적용

--- src/main/java/site/iplease/iadsmserver/domain/demand/advice/IpAssignDemandStatusControllerAdvice.kt
+++ src/main/java/site/iplease/iadsmserver/domain/demand/advice/IpAssignDemandStatusControllerAdvice.kt
@@ -9,30 +9,45 @@ import site.iplease.iadsmserver.domain.demand.exception.DemandStatusAlreadyExist
 import site.iplease.iadsmserver.domain.demand.exception.MalformedIpException
 import site.iplease.iadsmserver.domain.demand.exception.UnChangeableDemandStatusException
 import site.iplease.iadsmserver.domain.demand.exception.UnknownDemandStatusException
+import site.iplease.iadsmserver.global.error.ErrorResponse
+import site.iplease.iadsmserver.global.error.ErrorStatus
 
 @RestControllerAdvice
 class IpAssignDemandStatusControllerAdvice {
     @ExceptionHandler(DemandStatusAlreadyExistsException::class)
-    fun handle(exception: DemandStatusAlreadyExistsException): Mono<ResponseEntity<String>> =
+    fun handle(e: DemandStatusAlreadyExistsException): Mono<ResponseEntity<ErrorResponse>> =
         ResponseEntity.badRequest()
-            .body(exception.localizedMessage)
-            .toMono()
+            .body(ErrorResponse(
+                status = ErrorStatus.DEMAND_STATUS_ALREADY_EXISTS,
+                message = e.getErrorMessage(),
+                detail = e.getErrorDetail()
+            )).toMono()
 
     @ExceptionHandler(UnknownDemandStatusException::class)
-    fun handle(exception: UnknownDemandStatusException): Mono<ResponseEntity<String>> =
+    fun handle(e: UnknownDemandStatusException): Mono<ResponseEntity<ErrorResponse>> =
         ResponseEntity.badRequest()
-            .body(exception.localizedMessage)
-            .toMono()
+            .body(ErrorResponse(
+                status = ErrorStatus.UNKNOWN_DEMAND_STATUS,
+                message = e.getErrorMessage(),
+                detail = e.getErrorDetail()
+            )).toMono()
 
     @ExceptionHandler(UnChangeableDemandStatusException::class)
-    fun handle(exception: UnChangeableDemandStatusException): Mono<ResponseEntity<String>> =
+    fun handle(e: UnChangeableDemandStatusException): Mono<ResponseEntity<ErrorResponse>> =
         ResponseEntity.badRequest()
-            .body(exception.localizedMessage)
-            .toMono()
+            .body(ErrorResponse(
+                status = ErrorStatus.UNCHANGEABLE_DEMAND_STATUS,
+                message = e.getErrorMessage(),
+                detail = e.getErrorDetail()
+            )).toMono()
 
     @ExceptionHandler(MalformedIpException::class)
-    fun handle(exception: MalformedIpException): Mono<ResponseEntity<String>> =
+    fun handle(e: MalformedIpException): Mono<ResponseEntity<ErrorResponse>> =
         ResponseEntity.badRequest()
-            .body(exception.localizedMessage)
-            .toMono()
+            .body(ErrorResponse(
+                status = ErrorStatus.MALFORMED_IP,
+                message = e.getErrorMessage(),
+                detail = e.getErrorDetail()
+            )
+            ).toMono()
 }
